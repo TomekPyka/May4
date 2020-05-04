02dbdb0b7cd44ce0662caeeec9d3fbd502ed84b8
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-05-04 18:58:37 +0200

    Koniec projektu

--- May4/Program.cs
+++ May4/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace May4
 {
-    class Program
-    {
-         public static void Main(string[] args)
-        {
-            String str = "azor";
-            var httpClient = new HttpClient();
-            httpClient.GetAsync("https://www.pja.edu.pl");
+    class Program{
+         public static async Task Main(string[] args){
+            string str = "azor";
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://www.pja.edu.pl");
+            if (response.IsSuccessStatusCode) //2xx
+            {
+                string html = await response.Content.ReadAsStringAsync();
+                Regex regex = new Regex("[a-z0-9]+@[a-z0-9.]+");
+                MatchCollection matches = regex.Matches(html);
+                foreach (object i in matches)
+                {
+                    Console.WriteLine(i);
+                }
+            }
             Console.WriteLine(str);
             Console.WriteLine("Hello World!");
         }
