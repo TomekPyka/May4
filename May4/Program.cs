468f9a1884c9129da1c4ab9fa44d6561e53e8217
TomekPyka <61656933+TomekPyka@users.noreply.github.com>
2020-05-04 10:05:43 +0200

    Dodaj pliki projektów.

---
+++ May4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace May4
+{
+    class Program
+    {
+         public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
